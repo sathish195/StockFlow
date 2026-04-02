c5abc056faf7f0a2299e09b2978ab6d4be1d77dd
sathish195 <sathishdundigala999@gmail.com>
2026-04-02 10:47:10 +0530

    final commit

--- backend/src/helpers/schema_vallidations.js
+++ backend/src/helpers/schema_vallidations.js
@@ -16,7 +16,7 @@ module.exports = {
     loging: (data) => {
     const schema = Joi.object({
         email: Joi.string().email().lowercase().required(),
-        password: Joi.string().min(6).lowercase().required(),
+        password: Joi.string().min(6).required(),
       });
     return schema.validate(data);
   },
